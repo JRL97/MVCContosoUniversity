d241a0b948d6ed7e0976de1a56aedfe00dc093a3
JRL97 <91621546+JRL97@users.noreply.github.com>
2022-12-08 11:02:15 +0000

    Logbook 3

--- MVCContosoUniversity/Data/ApplicationDbContext.cs
+++ MVCContosoUniversity/Data/ApplicationDbContext.cs
@@ -15,11 +15,6 @@ namespace MVCContosoUniversity.Data
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Course>().ToTable("Course");
-            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
-            modelBuilder.Entity<Student>().ToTable("Student");
-        }
+      
     }
 }
--- MVCContosoUniversity/Program.cs
+++ MVCContosoUniversity/Program.cs
@@ -20,6 +20,15 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated();
+    DbInitializer.Initialize(context);
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
